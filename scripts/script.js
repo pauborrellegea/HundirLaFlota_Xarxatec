87d3f0f4c3220ec821a09836dbb842760e24a06e
Aaron <aaronbellesbeltran@gmail.com>
2021-11-08 17:19:38 +0100

    Ahora la pagina funciona por capas, primero se muestra la de Jugar, cuando se clica elimina esa capa y se muestra(en un futruro la creará) la capa de Seleccion de barcos por parte del jugador, al acabar y pulsar siguiente se va a la capa de juego.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,27 @@
 //funcion para crear tablas con document.createElement("tr") en el document.querySelector(#tableroJugador)
-
+//En la pantalla de selección de barcos, hacer un remove child de Ul->Li para pillar la primera posición
 
 
 import Barcos from "./barcos.js";
 
+document.getElementById("contenedorTableros").style.visibility="hidden";
+document.getElementById("SeleccionJugador").style.visibility="hidden";
+
 document.getElementById("botonJugar").addEventListener("click", function() {
-    const list = document.getElementById("boxJugar");
+    const list = document.getElementById("contenedorBoxJugar");
     list.parentNode.removeChild(list);
-    var x = document.getElementsByClassName("contenedorJugar");
+    document.getElementById("SeleccionJugador").style.visibility="visible";
+    var x = document.getElementsByClassName("contenedorJugar");    
     for (var i = 0; i<x.length; i++) {
         x[i].classList.remove("enfoque");
      }
   });
+document.getElementById("botonSiguiente").addEventListener("click", function() {
+    const list = document.getElementById("SeleccionJugador");
+    list.parentNode.removeChild(list);
+    document.getElementById("contenedorTableros").style.visibility="visible";
+});
+
     
 
 var vidaJugador = 4;
