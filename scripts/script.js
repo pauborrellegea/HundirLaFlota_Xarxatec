d756ab7eca26c0c2f65c85510ac0a7adb0f6addb
Aaron <aaronbellesbeltran@gmail.com>
2021-11-08 16:04:09 +0100

    Cambiado los tamaños de las tablas, añadida funcionalidad al boton de Jugar para que desaparezca la caja y elimine la clase Blur

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,15 @@
 
 import Barcos from "./barcos.js";
 
+document.getElementById("botonJugar").addEventListener("click", function() {
+    const list = document.getElementById("boxJugar");
+    list.parentNode.removeChild(list);
+    var x = document.getElementsByClassName("contenedorJugar");
+    for (var i = 0; i<x.length; i++) {
+        x[i].classList.remove("enfoque");
+     }
+  });
+    
 
 var vidaJugador = 4;
 var vidaIA = 4;
@@ -13,7 +22,6 @@ var turno;
 inicioPartida(10,10);
 jugar();
 
-function jugar(){
-
+function jugar(){    
     turno = 0;
 }
